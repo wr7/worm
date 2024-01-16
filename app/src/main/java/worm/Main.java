c948c439e6332da728d3b25961da5ddf17b57143
wr7 <d-wr7@outlook.com>
2024-01-15 22:41:49 -0600

    Add comments to Main.java

--- app/src/main/java/worm/Main.java
+++ app/src/main/java/worm/Main.java
@@ -1,9 +1,15 @@
+/*
+ * AP Computer Science A project
+ *
+ * Worm - A multi-level graphical Snakebird-like puzzle game
+*/
 package worm;
 
 import worm.graphics.WormWindow;
 
 public class Main {
     public static void main(String[] args) {
+        // Create window
         new WormWindow();
     }
 }
